75a2291c1d5551de98e72b6d95f0b4cb724db5f6
mkw18 <mkw22@mails.tsinghua.edu.cn>
2024-02-02 12:19:35 -0800

    unix time

--- app.py
+++ app.py
@@ -5,12 +5,36 @@ Change this to a world clock app
 import time
 
 import streamlit as st
+from datetime import datetime
+import pytz
 
-placeholder = st.empty()
+def world_clock():
+    st.title("World Clock")
+    def showTime():
+        while True:
+            with placeholder.container():
+                placeholder.metric('Time', datetime.now(pytz.timezone(st.session_state.timezone)).ctime())
+                timestamp.metric('UNIX timestamp', datetime.timestamp(datetime.now()))
+            time.sleep(1)
 
-cnt = 0
-while True:
-    with placeholder.container():
-        placeholder.metric("Seconds since you arrived this page", cnt)
-        cnt += 1
-    time.sleep(1)
+    st.selectbox('Time zone', ['US/Pacific', "US/Eastern", 'UTC', "Asia/Shanghai"], key='timezone', on_change=showTime)
+    placeholder = st.empty()
+    timestamp = st.empty()
+
+    showTime()
+
+def unix_converter():
+    st.title("Unix Converter")
+    unix_time_input = st.number_input("UNIX Timestamp:", min_value=0, step=1, value=int(datetime.timestamp(datetime.now())))
+    st.metric("Human Time - Your Time zone", datetime.fromtimestamp(unix_time_input).strftime('%Y-%m-%d %H:%M:%S'))
+    st.metric("Human Time - UTC", datetime.utcfromtimestamp(unix_time_input).strftime('%Y-%m-%d %H:%M:%S'))
+
+
+pages = {
+    "World Clock": world_clock,
+    "Unix Converter": unix_converter,
+}
+st.sidebar.title("Navigation")
+selected_page = st.sidebar.selectbox("Go to", list(pages.keys()))
+
+pages[selected_page]()
