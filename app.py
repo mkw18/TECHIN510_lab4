09609dcd749d60bbb93271abff4ca874d09bfe2a
mkw18 <mkw22@mails.tsinghua.edu.cn>
2024-02-02 13:19:16 -0800

    add hello

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 import streamlit as st
 from datetime import datetime
 import pytz
+import yfinance as yf
 
 def world_clock():
     st.title("World Clock")
@@ -29,10 +30,26 @@ def unix_converter():
     st.metric("Human Time - Your Time zone", datetime.fromtimestamp(unix_time_input).strftime('%Y-%m-%d %H:%M:%S'))
     st.metric("Human Time - UTC", datetime.utcfromtimestamp(unix_time_input).strftime('%Y-%m-%d %H:%M:%S'))
 
+def fetch_financial_data_page():
+    st.title("Real-time Stock Data")
+
+    symbol = st.text_input("Enter Stock Symbol (e.g., AAPL for Apple):")
+
+    if symbol:
+        stock = yf.Ticker(symbol)
+        data = stock.history(period='1d')
+        if data is not None:
+            st.write(f"Real-time Stock Data for {symbol}:")
+            st.write(data)
+        else:
+            st.write("No data available for the entered stock symbol.")
+    else:
+        st.write("No data available for the entered stock symbol.")
 
 pages = {
     "World Clock": world_clock,
     "Unix Converter": unix_converter,
+    "Realtime Stock Data": fetch_financial_data_page,
 }
 st.sidebar.title("Navigation")
 selected_page = st.sidebar.selectbox("Go to", list(pages.keys()))
